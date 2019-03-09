224c58529ff05e06ec79e85ce9587182cda4f0b5
Manjeet Saini <33650320+itsManjeet@users.noreply.github.com>
2019-03-09 14:40:48 +0530

    Wikipedia work start
    Initialization Done
    InfoCard Done
    Content Next

---
+++ Modules/Wiki.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import requests
+from bs4 import BeautifulSoup
+import sys
+_URL_ = "https://en.wikipedia.org/wiki/"
+
+# 2 : article not found on wikipedia
+#
+#
+# 5 : InfoCard is not present
+#
+
+class Wikipedia:
+    def Start(self,word):
+        page = requests.get(_URL_ + word)
+        self.soup = BeautifulSoup(page.text, 'html5lib')
+        self.content = self.soup.find('div',attrs={'class':'mw-body'})
+        if "Wikipedia does not have an article with this exact name" in self.content.text.strip():
+            return 2
+
+    def GetInfoCard(self):
+        infocard = self.content.find_all('table')
+        self.InfoCard = {}
+        for i in infocard:
+            try:
+                if i.get('class')[0] == 'infobox':
+                    for j in i.find_all('tr'):
+                        try:
+                            self.InfoCard[j.find('th').text.strip()] = j.find('td').text.strip()
+                        except AttributeError:
+                            pass
+            except TypeError as e:
+                if str(e) == "'NoneType' object is not subscriptable":
+                    return 5
+                
+        return 5
+
+    def GetContentHeadings(self):
+        
+
+Wiki = Wikipedia()
+
+Wiki.Start(sys.argv[1])
+Wiki.GetInfoCard()
+Wiki.GetContentHeadings()
