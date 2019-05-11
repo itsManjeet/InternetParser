9babf4e679cb80435b31cec77f2dd876ed93f798
itsmanjeet <sainishaab550@gmail.com>
2019-05-11 22:45:12 +0530

    new sample for wikipedia

--- wiki_sample.py
+++ wiki_sample.py
@@ -1,11 +1,10 @@
-import Modules.Wiki as wiki
+import Modules.Wikipedia as wikipedia
 
-if __name__ == "__main__":
-    Wi = wiki.Wikipedia()
-    if Wi.Start("Sundar pichai") == 2:
-        print("Unable to find information")
-    
-    print("== Sundar Pichai ==")
-    Wi.GetInfoCard()
-    for i in Wi.InfoCard:
-        print(i + " : " + Wi.InfoCard[i])
+if __name__ == '__main__':
+    wikipedia.init("sundar pichai")
+
+    if not wikipedia.exists():
+        print("Unable to find information regarding sundar pichai")
+        exit(0)
+
+    print(wikipedia.defination)
